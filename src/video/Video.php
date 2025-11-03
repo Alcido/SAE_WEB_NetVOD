ad5973161021d60b22c510125d5c4f722afcba4f
gpasques <germain.pasques4@etu.univ-lorraine.fr>
2025-11-03 17:19:12 +0100

    Ajout du diagramme UML

---
+++ src/video/Video.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace NetVOD\src\video;
+
+class Video
+{
+    // Attributs
+    private string $nom;
+    private string $lienFichier;
+    private ?string $lienImage;
+    private int $duree;
+    private int $annee;
+
+    /**
+     * @param string $nom nom de la video
+     * @param string $fichier lien vers le fichier de la video
+     * @param int $duree duree de la video
+     * @param int $annee annee de la video
+     */
+    public function __construct(string $nom, string $fichier, int $duree, int $annee, string $lienImage = null)
+    {
+        $this->nom = $nom;
+        $this->lienFichier = $fichier;
+        $this->lienImage = $lienImage;
+        $this->duree = $duree;
+        $this->annee = $annee;
+    }
+
+    /** Getter magique
+     * @param $attribut
+     * @return mixed valeur de l'attribut
+     */
+    public function __get($attribut) : mixed
+    {
+        if (property_exists($this, $attribut)) return $this->$attribut;
+        return null;
+    }
+}
