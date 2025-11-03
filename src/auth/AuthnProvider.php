e4d636c66abe7dbf862b9aab65f59cde3f3c8059
Matthieu Petelot <matthieu.petelot5@etu.univ-lorraine.fr>
2025-11-03 18:46:02 +0100

    [Add] ajout de l'utilisateur et de AuthnProvider pour la gestion de l'utilisateur (et ajout d'exception liée à cela)

---
+++ src/auth/AuthnProvider.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types=1);
+
+namespace NetVOD\src\auth;
+
+
+use NetVOD\src\Exception\AuthnException;
+use NetVOD\src\repository\Repository;
+
+/**
+ * Classe d'authentification
+ */
+class AuthnProvider
+{
+
+    /** Méthode de connexion
+     * @param string $mail mail de connexion
+     * @param string $mdp mot de passe de connexion
+     * @return void
+     * @throws AuthnException erreur de connexion
+     * @throws \Exception exception potentielle avec l'utilisation de PDO
+     */
+    public static function signin(string $mail, string $mdp) : void {
+        // On récupère l'utilisateur depuis la BDD
+        $user = Repository::getInstance()->getUser($mail);
+        // Si l'utilisateur n'existe pas
+        if (!$user) {
+            throw new AuthnException("Utilisateur n'existe pas");
+        }
+        // Si le mot de passe est invalide
+        if (!password_verify($mdp, $user['passwd'])) {
+            throw new AuthnException("Mot de passe incorrect");
+        }
+        // On ajoute l'utilisateur trouvé en session
+        $value = new User($user['id'], intval($user['role']), $user['pseudo']);
+        $_SESSION['user'] = serialize($value);
+    }
+
+    /** Méthode d'inscription d'un utilisateur
+     * @param string $mail mail d'inscription
+     * @param string $mdp mot de passe d'inscription
+     * @return void
+     * @throws AuthnException erreur d'inscription
+     */
+    public static function register(string $pseudo, string $mail, string $mdp) : void {
+        // On ajoute l'utilisateur à la BDD
+        $user = Repository::getInstance()->createUser($pseudo, $mail, $mdp);
+        // Si l'inscription s'est mal passée
+        if ($user === false) {
+            throw new AuthnException("Identifiant existe déja");
+        }
+    }
+
+}
---
+++ src/auth/User.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+namespace NetVOD\src\auth;
+
+class User
+{
+    private int $id;
+    private int $role;
+    private string $pseudo;
+
+    /**
+     * @param int $id id de l'utilisateur
+     * @param int $role role de l'utilisateur
+     * @param string $pseudo pseudo de l'utilisateur
+     */
+    public function __construct(int $id, int $role, string $pseudo) {
+        $this->id = $id;
+        $this->role = $role;
+        $this->pseudo = $pseudo;
+    }
+
+    /**
+     * @param string $name nom de l'attribut recherché
+     * @return mixed valeur de l'attribut recheché
+     * @throws \Exception si l'attribut n'existe pas, retourne une exception
+     */
+    public function __get(string $name) : mixed{
+        if (property_exists($this, $name)){
+            return $this->$name;
+        } else {
+            throw new \Exception("Property $name does not exist");
+        }
+    }
+
+}
