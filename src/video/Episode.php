c95b364759b2322c616d52b7acda41cfe7f61f3c
gpasques <germain.pasques4@etu.univ-lorraine.fr>
2025-11-04 15:03:03 +0100

    [Modify] modifications episode video

--- src/video/Episode.php
+++ src/video/Episode.php
@@ -11,14 +11,14 @@ class Episode extends Video
      * @param string $nom
      * @param string $fichier
      * @param int $duree
-     * @param int $annee
+
      * @param string $nom_serie
      * @param string|null $lienImage
      * @param string $resume
      */
-    public function __construct(string $nom, string $fichier, int $duree, int $annee, string $nom_serie, int $numero, string $resume, ?string $lienImage = null)
+    public function __construct(string $nom, string $fichier, int $duree, string $nom_serie, int $numero, string $resume, ?string $lienImage = null)
     {
-        parent::__construct($nom, $fichier, $duree, $annee, $resume, $lienImage);
+        parent::__construct($nom, $fichier, $duree, $resume, $lienImage);
         $this->nom_serie = $nom_serie;
         $this->numero = $numero;
     }
