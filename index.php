76fe22ae0e418f0d6a8b31106f761cc2bc43f66d
Arthur JAcquet <arthur.jacquet2@etu.univ-lorraine.fr>
2025-11-05 10:46:26 +0100

    ajout de affichage erreur php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
+
+
 declare(strict_types=1);
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 require_once 'vendor/autoload.php';
 use NetVOD\src\dispatcher\Dispatcher;
 use NetVOD\src\repository\Repository;
