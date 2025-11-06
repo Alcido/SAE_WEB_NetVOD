33ee04c9a3a5441c9b2c964fff4da1b565b7443d
Arthur JAcquet <arthur.jacquet2@etu.univ-lorraine.fr>
2025-11-06 14:46:20 +0100

    modif affichage info

--- src/action/ActionAffichageInfos.php
+++ src/action/ActionAffichageInfos.php
@@ -20,20 +20,40 @@ class ActionAffichageInfos extends Action
 
         if (!$infos) {
             return <<<HTML
-            <div class="profil-infos">
-                <h2>Mes informations personnelles</h2>
-                <ul>
-                    <li><a href="?action=add-infos&value=nom"><button>edit</button></a><strong>Nom :</strong> <a href="?action=dell-infos&value=nom"><button>del</button></li>
-                    <li><a href="?action=add-infos&value=prenom"><button>edit</button><strong>Prénom :</strong> <a href="?action=dell-infos&value=prenom"><button>del</button></li>
-                    <li><a href="?action=add-infos&value=genre"><button>edit</button><strong>Genre :</strong> <a href="?action=dell-infos&value=genre"><button>del</button></li>
-                    <li><a href="?action=add-infos&value=birth_date"><button>edit</button><strong>Date de naissance :</strong> <a href="?action=dell-infos&value=birth_date"><button>del</button></li>
-                    <li><a href="?action=add-infos&value=adresse"><button>edit</button><strong>Adresse :</strong> <a href="?action=dell-infos&value=adresse"><button>del</button></li>
-                </ul>
-                <br>
-            </div>
-        HTML;
-
+    <div class="profil-infos">
+        <h2>Mes informations personnelles</h2>
+        <ul>
+            <li>
+                <a href="?action=add-infos&value=nom"><button type="button" class="btn-edit">✏️</button></a>
+                <strong>Nom :</strong>
+                <a href="?action=dell-infos&value=nom"><button type="button" class="btn-del">🗑️</button></a>
+            </li>
+            <li>
+                <a href="?action=add-infos&value=prenom"><button type="button" class="btn-edit">✏️</button></a>
+                <strong>Prénom :</strong>
+                <a href="?action=dell-infos&value=prenom"><button type="button" class="btn-del">🗑️</button></a>
+            </li>
+            <li>
+                <a href="?action=add-infos&value=genre"><button type="button" class="btn-edit">✏️</button></a>
+                <strong>Genre :</strong>
+                <a href="?action=dell-infos&value=genre"><button type="button" class="btn-del">🗑️</button></a>
+            </li>
+            <li>
+                <a href="?action=add-infos&value=birth_date"><button type="button" class="btn-edit">✏️</button></a>
+                <strong>Date de naissance :</strong>
+                <a href="?action=dell-infos&value=birth_date"><button type="button" class="btn-del">🗑️</button></a>
+            </li>
+            <li>
+                <a href="?action=add-infos&value=adresse"><button type="button" class="btn-edit">✏️</button></a>
+                <strong>Adresse :</strong>
+                <a href="?action=dell-infos&value=adresse"><button type="button" class="btn-del">🗑️</button></a>
+            </li>
+        </ul>
+        <br>
+    </div>
+    HTML;
         }
+
 
 
         $nom        = htmlspecialchars($infos['nom'] ?? '', ENT_QUOTES, 'UTF-8');
